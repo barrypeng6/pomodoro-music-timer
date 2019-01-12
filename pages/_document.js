ff8408c9aab7c29243bb482b5a1938f92d9bbfb5
Barry Peng <barrypeng6@hotmail.com>
2019-01-12 23:40:05 +0800

    feat: add ga

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,20 @@ export default class MyDocument extends Document {
     return (
       <html lang="en-US">
         <Head>
+          <title>Pomodoro Music Timer</title>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-132387945-1"
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-132387945-1');
+            `}
+          </script>
+
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
@@ -39,7 +53,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <script src="https://www.youtube.com/player_api" />
+          <script async src="https://www.youtube.com/player_api" />
           <NextScript />
         </body>
       </html>
